bdd144134cb30899e498401ea0d4c05f47de97e8
정원 <66169832+JayeHa@users.noreply.github.com>
2023-02-02 01:29:52 +0900

    💎 style: extract .list-dotted style from ProjectExperience

--- apps/resume/app/components/Sections/WorkExperience/WorkExperienceCard/ProjectExperience/ProjectExperience.tsx
+++ apps/resume/app/components/Sections/WorkExperience/WorkExperienceCard/ProjectExperience/ProjectExperience.tsx
@@ -26,23 +26,16 @@ const ProjectExperience = ({ project }: ProjectExperienceProps) => {
         {skills.map((skill) => (
           <li
             key={skill}
-            className=" rounded bg-gray-200 py-0.5 px-2 text-xs text-gray-800"
+            className="rounded bg-gray-200 py-0.5 px-2 text-xs text-gray-800"
           >
             {skill}
           </li>
         ))}
       </ul>
 
-      <ul className="mt-2">
+      <ul className="list-dotted mt-2">
         {description.map((desc) => (
-          <li
-            key={desc}
-            className="relative pl-3 text-[0.95rem] last:mb-0 md:mb-1 md:text-base"
-          >
-            <span
-              className="absolute top-3 left-0 block h-1 w-1 rounded-full bg-gray-800"
-              aria-hidden
-            />
+          <li key={desc} className="text-[0.95rem] md:text-base">
             {desc}
           </li>
         ))}
