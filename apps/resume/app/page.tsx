f12a3ea7edae64e30867f6d4d2d49aea2effc692
정원 <66169832+JayeHa@users.noreply.github.com>
2023-02-01 23:42:22 +0900

    🧹 chore(config): add prettier-plugin-tailwindcss and prettier.config.js

--- apps/resume/app/page.tsx
+++ apps/resume/app/page.tsx
@@ -1,12 +1,13 @@
-import { Cover, Experience, Skills } from "./components";
+import { Cover, Experience, Projects, Skills } from "./components";
 
 export default function Home() {
   return (
-    <main className="px-4 py-12 md:max-w-5xl md:px-24 md:py-32 md:mx-auto">
+    <main className="px-4 py-12 md:mx-auto md:max-w-5xl md:px-24 md:py-32">
       <Cover />
 
       <Skills />
       <Experience />
+      <Projects />
     </main>
   );
 }
