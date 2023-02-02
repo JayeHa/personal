ef83a7849e8baf6082aab9de566f67aa26cc6a49
정원 <66169832+JayeHa@users.noreply.github.com>
2023-02-03 01:18:48 +0900

    🧹 chore(config): add image hostname from next.config.js
    add github.com to next.config.js

--- apps/resume/next.config.js
+++ apps/resume/next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
