967026c89f6ebcf74fd1d6d503df03aa7d4eaf0c
정원 <66169832+JayeHa@users.noreply.github.com>
2023-02-03 21:53:13 +0900

    🧹 chore(resume): add data to openSourceList and remove sideProjectList from Projects

--- apps/resume/app/components/Sections/Projects/Projects.constants.ts
+++ apps/resume/app/components/Sections/Projects/Projects.constants.ts
@@ -1,13 +1,10 @@
 export const OpenSourceList = [
   {
+    name: "Mantine",
+    link: "https://github.com/mantinedev/mantine/pulls?q=is%3Apr+author%3AJayeHa",
+  },
+  {
     name: "Falso | All the Fake Data for All Your Real Needs",
     link: "https://github.com/ngneat/falso/pulls?q=is%3Apr+author%3AJayeHa",
-  },
-];
-
-export const SideProjectList = [
-  {
-    name: "포트폴리오 웹 사이트",
-    link: "https://github.com/JayeHa/personal",
   },
 ];
--- apps/resume/app/components/Sections/Projects/Projects.tsx
+++ apps/resume/app/components/Sections/Projects/Projects.tsx
@@ -1,12 +1,11 @@
 import Section from "../Section";
 import ProjectList from "./ProjectList";
-import { OpenSourceList, SideProjectList } from "./Projects.constants";
+import { OpenSourceList } from "./Projects.constants";
 
 const Projects = () => {
   return (
     <Section title="Projects">
       <ProjectList title="오픈소스 프로젝트" projectList={OpenSourceList} />
-      <ProjectList title="사이드 프로젝트" projectList={SideProjectList} />
     </Section>
   );
 };
