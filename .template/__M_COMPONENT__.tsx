0f19e425e00250b3d713a19ac6115292f03d3d15
정원 <66169832+JayeHa@users.noreply.github.com>
2023-02-12 20:05:16 +0900

    🧹 chore: update .template

--- .template/__M_COMPONENT__.tsx
+++ .template/__M_COMPONENT__.tsx
@@ -1,11 +1,10 @@
 import use__M_COMPONENT__ from "./__M_COMPONENT__.hooks";
-import * as Styled from "./__M_COMPONENT__.styles";
 import type __M_COMPONENT__Props from "./__M_COMPONENT__.types";
 
 const __M_COMPONENT__ = ({ dummy }: __M_COMPONENT__Props) => {
   const app = use__M_COMPONENT__();
 
-  return <Styled.Container>hi</Styled.Container>;
+  return <div>hi</div>;
 };
 
 export default __M_COMPONENT__;
