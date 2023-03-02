0cf06ab00a573b0c37370f4751e055bb789d0968
정원 <66169832+JayeHa@users.noreply.github.com>
2023-03-02 21:44:55 +0900

    ✨ feat(resume): implement useResponsive hook

---
+++ apps/resume/app/hooks/useResponsive.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { BREAKPOINT_MOBILE } from "~/constants";
+
+const useResponsive = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const updateMatchMedia = ({ matches }: MediaQueryListEvent) => {
+      setIsMobile(matches);
+    };
+
+    const media = window.matchMedia(`(max-width: ${BREAKPOINT_MOBILE})`);
+    media.addEventListener("change", updateMatchMedia);
+    setIsMobile(media.matches);
+
+    return () => media.removeEventListener("change", updateMatchMedia);
+  }, []);
+
+  return { isMobile };
+};
+
+export default useResponsive;
