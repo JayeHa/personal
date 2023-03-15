d078ab3f16a551167c6298bd7619aa3625463a7c
정원 <jwha94@gmail.com>
2023-03-15 11:04:02 +0900

    🧹 chore(resume): update workExperience data

--- apps/resume/app/components/Sections/WorkExperience/WorkExperience.constants.ts
+++ apps/resume/app/components/Sections/WorkExperience/WorkExperience.constants.ts
@@ -5,28 +5,9 @@ export const workExperiences = [
     period: "2022.10 - 현재",
     projects: [
       {
-        title: "짤툰대난투 소개페이지",
-        url: "https://jjaltoonrumble.bigpicture-games.com/",
-        subtitle: "자사 게임 서비스인 짤툰대난투를 소개하는 웹 페이지입니다.",
-        skills: [
-          "Next.js",
-          "TypeScript",
-          "Redux",
-          "React-query",
-          "Styled-component",
-        ],
-        description: [
-          "프론트엔드 인터페이스 구현",
-          "반응형 디자인 구현",
-          "Toast WYSIWYG",
-          "무한스크롤 기능 구현",
-          "인게임 페이지 구현",
-          "다국어 지원 대응 🇰🇷🇬🇧",
-        ],
-      },
-      {
         title: "몰라몰라 개복치",
-        subtitle: "각종 짤뽑기, 테스트, 게임 등",
+        subtitle: `React와 Next.js을 기반으로한 각종 유형테스트, 짤뽑기 등을 할 수 있는 자사 웹 서비스의 프론트엔드 개발을 담당했습니다.
+        개발 초기 단계의 공통 컴포넌트 작업을 시작으로 프로젝트 전반에 걸친 프론트엔드 개발을 맡고 있고, 주로 컨텐츠 제작과 관련된 업무를 하고 있습니다.`,
         skills: [
           "Next.js",
           "TypeScript",
@@ -37,14 +18,37 @@ export const workExperiences = [
         description: [
           "프론트엔드 인터페이스 구현",
           "초기 공통 컴포넌트 작업 (기여도 100%)",
+          "컨텐츠 개발(유형테스트 구현)",
+          "관리자페이지",
           "다국어 지원 대응 🇰🇷🇬🇧",
-          "테스트 코드 작성",
         ],
       },
       {
+        title: "짤툰대난투 소개페이지",
+        url: "https://jjaltoonrumble.bigpicture-games.com/",
+        subtitle: `자사 게임 서비스인 짤툰대난투를 소개하는 웹 페이지의 프론트엔드를 개발하며 공지사항﹒이벤트 등 게시판 작업을 진행하였고, 인게임 페이지를 맡아 자사 게임사업팀의 비지니스 요구에 따른 작업을 처리하였습니다.`,
+        skills: [
+          "Next.js",
+          "TypeScript",
+          "Redux",
+          "React-query",
+          "Styled-component",
+        ],
+        description: [
+          "프론트엔드 인터페이스 구현",
+          "반응형 디자인 구현",
+          "SSR환경에서 Toast WYSIWYG 세팅",
+          "무한스크롤 기능 구현",
+          "인게임 페이지 구현",
+          "다국어 지원 대응 🇰🇷🇬🇧",
+        ],
+      },
+
+      {
         title: "노담캠페인",
         url: "https://nodam.kr/",
-        subtitle: "보건복지부에서 진행하는 금연 캠페인",
+        subtitle:
+          "보건복지부에서 진행하는 금연 캠페인으로, 금빛조연 및 노담서포터즈 등 웹 페이지의 프론트엔드를 개발했습니다.",
         skills: [
           "Next.js",
           "TypeScript",
@@ -67,10 +71,12 @@ export const workExperiences = [
       {
         title: "코딩엑스",
         url: "https://coding-x.com/",
-        subtitle: "AI 학습 플랫폼",
+        subtitle:
+          "AI 학습 플랫폼의 프론트엔드를 담당하며 영상을 플레이할 수 있는 플레이룸을 시작으로, 수료증기능, Zoom연동, 이벤트 홍보용 웹사이트 등을 개발했습니다.",
         skills: ["EJS", "Javascript", "SCSS"],
         description: [
           "수료증 기능 제작",
+          "플레이룸 기능 개발",
           "반응형 디자인 구현",
           "웨비나 및 각종 이벤트 페이지 구현",
         ],
@@ -85,9 +91,14 @@ export const workExperiences = [
       {
         title: "입시내비게이션",
         url: "https://m.ipsinavi.com/",
-        subtitle: "입시 프로그램",
+        subtitle:
+          "학생들의 수시/정시 성적을 바탕으로 대학별 합격/불합격을 판단하는 입시배치정보 웹 프로그램의 오래된 UI/UX를 전면적으로 수정하고 리뉴얼 작업을 진행하였습니다.",
         skills: ["HTML", "CSS", "Javascript"],
-        description: ["반응형 디자인 구현"],
+        description: [
+          "반응형 디자인 구현",
+          "학원 요청사항에 따른 UI/UX 개선",
+          "RDMS 연동 비지니스 로직 및 공통 함수 모듈화 적용 및 생성에 참여",
+        ],
       },
     ],
   },
