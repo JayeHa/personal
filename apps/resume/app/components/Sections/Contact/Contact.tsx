e99e55f6b97b207629ce075dc93aa4bdec6757b9
정원 <66169832+JayeHa@users.noreply.github.com>
2023-03-18 18:57:53 +0900

    ♻️ refactor(resume): extract data from contact

--- apps/resume/app/components/Sections/Contact/Contact.tsx
+++ apps/resume/app/components/Sections/Contact/Contact.tsx
@@ -1,19 +1,17 @@
+import { contactList } from "@data/contact";
 import Section from "../Section";
 
 const Contact = () => {
   return (
     <Section title="Contact">
       <ul className="list-dotted">
-        <li className="mb-2">
-          <a href="https://github.com/JayeHa" target="_blank" rel="noreferrer">
-            깃허브
-          </a>
-        </li>
-        <li>
-          <a href="mailto:jwha94@gmail.com" target="_blank" rel="noreferrer">
-            이메일
-          </a>
-        </li>
+        {contactList.map((contact) => (
+          <li className="mb-2" key={contact.title}>
+            <a href={contact.link} target="_blank" rel="noreferrer">
+              {contact.title}
+            </a>
+          </li>
+        ))}
       </ul>
     </Section>
   );
---
+++ apps/resume/data/contact.ts
@@ -0,0 +1,4 @@
+export const contactList = [
+  { title: "깃허브", link: "https://github.com/JayeHa" },
+  { title: "이메일", link: "mailto:jwha94@gmail.com" },
+];
