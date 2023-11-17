a616d8a28793fa8c28e9ea88e68abeb62894ff2d
정원 <66169832+JayeHa@users.noreply.github.com>
2023-11-17 11:39:03 +0900

    chore: update data of workExperience

--- apps/resume/data/workExperience.ts
+++ apps/resume/data/workExperience.ts
@@ -6,15 +6,11 @@ export const workExperiences = [
     projects: [
       {
         title: "재고관리 서비스",
-        subtitle: `프론트엔드 개발자로서 진보상사에서 유통하는 상품들의 재고 관리를 위한 서비스의 개발을 진행했습니다.
-        개발 과정 중에 팀에 합류하였고, 재고관리 서비스에서 가장 핵심적인 컴포넌트가 Table이라고 판단하여 이를 우선적으로 리펙토링하여 생산성을 향상시켰습니다.
-        현재 프론트엔드를 리드하여 업무환경을 개선하고 프로젝트를 진행하고 있습니다.`,
         skills: ["Next.js", "TypeScript", "React-query", "Zustand", "emotion"],
         description: [
-          "재고관리 서비스의 프론트엔드 개발 및 리드",
           "Table 컴포넌트를 ContextAPI로 리펙토링하여 개발시간 50% 단축",
-          "프로젝트의 전반적인 생산성 향상",
           "불필요한 전역상태 제거 및 스프린트 도입으로 업무 효율성 증대",
+          "기획 단계에서의 요구사항 변경이 잦아짐에 따라 Jest를 활용한 테스트 코드를 도입하여 프로젝트의 안정성 향상",
         ],
       },
     ],
@@ -26,7 +22,6 @@ export const workExperiences = [
     projects: [
       {
         title: "몰라몰라 개복치",
-        subtitle: `React와 Next.js을 기반으로한 각종 유형테스트, 짤뽑기 등을 할 수 있는 자사 웹 서비스의 프론트엔드 개발을 담당했습니다. 개발 초기 단계의 공통 컴포넌트 작업을 시작으로 프로젝트 전반에 걸친 프론트엔드 개발을 맡았고, 주로 컨텐츠 템플릿 기능과 함께 관리자 페이지와 관련된 업무를 진행했습니다.`,
         skills: [
           "Next.js",
           "TypeScript",
@@ -42,7 +37,6 @@ export const workExperiences = [
       {
         title: "짤툰대난투 소개페이지",
         url: "https://jjaltoonrumble.bigpicture-games.com/",
-        subtitle: `자사 게임 서비스인 짤툰대난투를 소개하는 웹 페이지의 프론트엔드를 개발하며 공지사항﹒이벤트 등 게시판 작업을 진행하였고, 인게임 페이지를 맡아 자사 게임사업팀의 비지니스 요구에 따른 작업을 처리하였습니다.`,
         skills: [
           "Next.js",
           "TypeScript",
@@ -61,8 +55,6 @@ export const workExperiences = [
       {
         title: "노담캠페인",
         url: "https://nodam.kr/",
-        subtitle:
-          "보건복지부에서 진행하는 금연 캠페인으로, 금빛조연 및 노담서포터즈 등 웹 페이지의 프론트엔드를 개발했습니다.",
         skills: [
           "Next.js",
           "TypeScript",
@@ -85,8 +77,6 @@ export const workExperiences = [
       {
         title: "코딩엑스",
         url: "https://coding-x.com/",
-        subtitle:
-          "AI 학습 플랫폼의 프론트엔드를 담당하며 영상을 플레이할 수 있는 플레이룸을 시작으로 수료증기능 · Zoom연동 · 이벤트 홍보용 웹사이트 등을 개발했습니다. 특히 프로덕트 디자이너와의 협업으로 디자인시스템을 구축하였으며, HTML 명세에 맞는 시맨틱 태그들로 개선하고 기존 SCSS 코드의 복잡도를 낮추었습니다.",
         skills: ["EJS", "Javascript", "SCSS"],
         description: [
           "디자인 시스템 구축 및 시맨틱, 반응형 디자인 구현을 통한 Lighthouse 점수 향상 (Accessibility 93점, SEO 100점, Performance 98점)",
